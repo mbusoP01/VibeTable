151730ba168acd879f3fd71e14b4efbca9fbc699
mbusoP01 <mbusophiri01@gmail.com>
2026-02-06 04:01:39 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'vibetable-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Assets (Serve from Cache if offline)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
